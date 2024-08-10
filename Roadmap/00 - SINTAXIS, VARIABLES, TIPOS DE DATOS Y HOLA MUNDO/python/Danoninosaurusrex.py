4220e4fe80e39382077d39a8d59f97a77748c34d
Danoninosaurusrex <pedrochonay555@gmail.com>
2024-08-09 19:18:22 -0600

    Create Danoninosaurusrex.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Danoninosaurusrex.py
@@ -0,0 +1,20 @@
+# https://www.python.org
+
+#comentario OwO
+
+"""
+comentarios de varias líneas pa aprender a 
+programar
+y ser millonario en 3 años
+"""
+
+'''
+comentaio 
+o no se
+'''
+
+variable = "naranjas"
+variable = "manzanas"
+
+CONSTANTE = "precio" #no existen constantes pero por convención se utilizan variables pero fijandolas 
+CONSTANTE = "vendidos"
